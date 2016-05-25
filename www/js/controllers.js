236036bf321cea1d31abb6dab05adb7824888a14
axel laurelut <axel.laurelut@gmail.com>
2016-05-25 16:24:38 +0200

    refresh data

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,8 +4,34 @@ angular.module('starter.controllers', [])
   User
   .getMeasure()
   .then(function(data) {
-    console.log(data);
-    $scope.user = data;
+    var measuregrps = data.body.measuregrps;
+    $scope.user = {};
+
+    for (var i = (measuregrps.length - 1); i >= 0; i--) {
+
+      for (var y = 0; y < measuregrps[i].measures.length; y++) {
+        var measure = measuregrps[i].measures[y];
+        switch (measure.type) {
+          case 1:
+            $scope.user.weight = (measure.value / 10);
+            break;
+          case 4:
+            $scope.user.height = measure.value;
+            break;
+          case 9:
+            $scope.user.diastolicBloodPressure = measure.value;
+            break;
+          case 10:
+            $scope.user.sistolicBloodPressure = measure.value;
+            break;
+          case 11:
+            $scope.user.heartPulse = measure.value;
+            break;
+          default:
+
+        }
+      }
+    }
   });
 })
 
